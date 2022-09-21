78da3d8991f663bdb554792359b6dca5b9c5ef6b
Diego Palomino <diegof.palomino@gmail.com>
2022-09-20 21:02:25 -0400

    primer commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.listen(process.env.PORT || 3000, () => console.log('Servidor corriendo en el puerto 3000'));
+
+app.get('/', (req, res) => { 
+   res.sendFile(path.resolve(__dirname, './src/views/home.html'));
+   
+});
+
+
+app.get('/register', (req, res) => { 
+   res.sendFile(path.resolve(__dirname, './src/views/register.html'));
+   
+});
+
+app.post('/register', (req, res) => { 
+   res.sendFile(path.resolve(__dirname, './src/views/register.html'));
+   
+});
+
+app.get('/login', (req, res) => { 
+   res.sendFile(path.resolve(__dirname, './src/views/login.html'));
+   
+});
+
+app.post('/login', (req, res) => { 
+   res.sendFile(path.resolve(__dirname, './src/views/login.html'));
+   
+});
+
